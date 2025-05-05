ed2d610116f08e9c740c14690ece115f6b52e606
xSurfuel2 <xSurfuel2@gmail.com>
2025-05-05 10:21:25 +0200

    12

--- Proyecto_clase/src/View/GestionarEquipos.java
+++ Proyecto_clase/src/View/GestionarEquipos.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class GestionarEquipos extends JFrame {
 
@@ -14,29 +16,25 @@ public class GestionarEquipos extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GestionarEquipos frame = new GestionarEquipos();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the frame.
 	 */
 	public GestionarEquipos() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 800, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null);//centra la ventana
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("GESTIONAR EQUIPOS");
+		lblNewLabel.setFont(new Font("Baskerville Old Face", Font.PLAIN, 38));
+		lblNewLabel.setBounds(158, 30, 422, 33);
+		contentPane.add(lblNewLabel);
 	}
 
 }
