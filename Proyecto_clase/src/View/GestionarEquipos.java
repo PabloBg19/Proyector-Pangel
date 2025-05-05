ea542ef664f939894f6932fa5cf50eae8eef94fe
xSurfuel2 <xSurfuel2@gmail.com>
2025-05-05 10:22:43 +0200

    12

--- Proyecto_clase/src/View/GestionarEquipos.java
+++ Proyecto_clase/src/View/GestionarEquipos.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
 
 public class GestionarEquipos extends JFrame {
 
@@ -35,6 +36,22 @@ public class GestionarEquipos extends JFrame {
 		lblNewLabel.setFont(new Font("Baskerville Old Face", Font.PLAIN, 38));
 		lblNewLabel.setBounds(158, 30, 422, 33);
 		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Ver Pilotos");
+		btnNewButton.setBounds(77, 193, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Modificar Pilotos");
+		btnNewButton_1.setBounds(258, 193, 119, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(437, 193, 89, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(600, 193, 89, 23);
+		contentPane.add(btnNewButton_3);
 	}
 
 }
